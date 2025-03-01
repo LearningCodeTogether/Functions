953af8f65969ddae4db022ef9f38a6ef5e030465
bdipanfilo <bdipanfilo724@aol.com>
2025-03-01 15:17:56 -0500

    Part 1 Solved

--- message_part1.py
+++ message_part1.py
@@ -2,4 +2,23 @@
 
 code = ([65,74,66,68,70],[74,96,68,80,117],[104,106,78,80,115],[116,74,96,68,80])
 def message1():
-    print("Nice Try")
+    answer = ""
+
+    z = 0
+
+    for z in range(len(code)):
+        x = 0
+        y = 4
+        while x != y:
+            if code[z][x]<code[z][y]:
+             x = x + 1
+
+            elif code[z][x]>code[z][y]:
+              y = y - 1
+        letter = chr(code[z][x])
+        answer = answer + letter
+        # print(code[z][x], "is the greatest value"
+        z = z + 1
+
+    print(answer)
+
